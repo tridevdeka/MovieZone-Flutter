140e4fafbe427339fe4b623823a76a05749c5177
tridev_deka <tridev.d@branchx.in>
2023-08-21 00:19:11 +0530

    get_it Dependency Injection

--- lib/domain/usecases/usecase.dart
+++ lib/domain/usecases/usecase.dart
@@ -1,7 +1,6 @@
 import 'package:dartz/dartz.dart';
 
 import '../entities/app_error.dart';
-import '../entities/movie_entity.dart';
 
 abstract class UseCase<Type, Params> {
   Future<Either<AppError, Type>> call(Params params);
--- lib/main.dart
+++ lib/main.dart
@@ -1,40 +1,45 @@
+import 'dart:async';
 import 'package:dartz/dartz.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
-import 'package:tmdb_movies_flutter/data/core/api_client.dart';
-import 'package:tmdb_movies_flutter/data/data_sources/movie_remote_data_source.dart';
-import 'package:tmdb_movies_flutter/data/repositories/movie_repository.dart';
-import 'package:tmdb_movies_flutter/data/repositories/movie_repository_impl.dart';
-import 'package:tmdb_movies_flutter/domain/entities/app_error.dart';
-import 'package:tmdb_movies_flutter/domain/entities/movie_entity.dart';
-import 'package:tmdb_movies_flutter/domain/entities/no_params.dart';
-import 'package:tmdb_movies_flutter/domain/usecases/get_trending.dart';
+import 'di/get_it.dart' as getIt;
+import 'domain/entities/app_error.dart';
+import 'domain/entities/movie_entity.dart';
+import 'domain/entities/no_params.dart';
+import 'domain/usecases/get_trending.dart';
 
 Future<void> main() async {
-  ApiClient apiClient = ApiClient(Client());
+  /*ApiClient apiClient = ApiClient(Client());
 
   MovieRemoteDataSource remoteDataSource = MovieRemoteDataSourceImpl(apiClient);
   MovieRepository movieRepository = MovieRepositoryImpl(remoteDataSource);
-  GetTrending getTrending = GetTrending(movieRepository);
+  GetTrending getTrending = GetTrending(movieRepository);*/
+  unawaited(getIt.init());
+  GetTrending getTrending = getIt.getItInstance<GetTrending>();
   final Either<AppError, List<MovieEntity>?> eitherResponse =
       await getTrending(NoParams());
-
-  eitherResponse.fold((l) {
-    print('error');
-    print(l);
-  }, (r) {
-    print('list of movies');
-    print(r);
-  });
-
+  eitherResponse.fold(
+    (l) {
+      print('error');
+      print(l);
+    },
+    (r) {
+      print('list of movies');
+      print(r);
+    },
+  );
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
   }
 }
